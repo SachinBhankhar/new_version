7e11df46c343e11ce8c3036742039fb72a40b009
Sachin Bhankhar <sachinbhankhar2000@gmail.com>
2022-08-19 22:09:20 +0530

    Make compatible with new Play Store UI

--- lib/new_version.dart
+++ lib/new_version.dart
@@ -150,8 +150,8 @@ class NewVersion {
   Future<VersionStatus?> _getAndroidStoreVersion(
       PackageInfo packageInfo) async {
     final id = androidId ?? packageInfo.packageName;
-    final uri = Uri.https(
-        "play.google.com", "/store/apps/details", {"id": "$id", "hl": "en"});
+    final uri =
+        Uri.https("play.google.com", "/store/apps/details", {"id": "$id"});
     final response = await http.get(uri);
     if (response.statusCode != 200) {
       debugPrint('Can\'t find an app in the Play Store with the id: $id');
